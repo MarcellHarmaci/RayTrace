50901614151f03c97a7959c125e44f656faa2697
Marcell Harmaci <harmaci.marci@gmail.com>
2020-04-19 01:37:19 +0200

    OO cleanup

--- Skeleton/raytrace.cpp
+++ Skeleton/raytrace.cpp
@@ -30,16 +30,11 @@ public:
 	virtual Hit intersect(const Ray& ray) = 0;
 };
 
-struct Quadrics : public Intersectable {
+class Quadric : public Intersectable {
+protected:
 	mat4 Q;
-	//vec3 param;
 
-	Quadrics(){}
-	Quadrics(mat4 _Q) {//, vec3 _param) { 
-		Q = _Q;
-		//param = _param; 
-	}
-
+public:
 	// Source: Homework assignment video
 	float f(vec4 r) {
 		return dot(r * Q, r);
@@ -54,13 +49,6 @@ struct Quadrics : public Intersectable {
 	vec2 solve(const Ray& ray) {
 		vec4 p = vec4(ray.start.x, ray.start.y, ray.start.z, 1.0f);
 		vec4 u = vec4(ray.dir.x, ray.dir.y, ray.dir.z, 0.0f);
-		//printf("p1: %3.2f, p2: %3.2f, p3: %3.2f, p4: %3.2f\n", p.x, p.y, p.z, p.w);
-		//printf("u1: %3.2f, u2: %3.2f, u3: %3.2f, u4: %3.2f\n", u.x, u.y, u.z, u.w);
-		//printf("Q:\n%3.2f\t%3.2f\t%3.2f\t%3.2f\n%3.2f\t%3.2f\t%3.2f\t%3.2f\n%3.2f\t%3.2f\t%3.2f\t%3.2f\n%3.2f\t%3.2f\t%3.2f\t%3.2f\n",
-		//	Q[0].x, Q[0].y, Q[0].z, Q[0].w,
-		//	Q[1].x, Q[1].y, Q[1].z, Q[1].w,
-		//	Q[2].x, Q[2].y, Q[2].z, Q[2].w,
-		//	Q[3].x, Q[3].y, Q[3].z, Q[3].w);
 
 		float a = dot(u * Q, u);
 		float b = dot(u * Q, p);
@@ -81,134 +69,74 @@ struct Quadrics : public Intersectable {
 		Hit hit;
 
 		vec2 solution = solve(ray);
-		float t1 = solution.x;
-		float t2 = solution.y;
-		if (t1 <= 0) return hit;
+		if (solution.x <= 0) return hit;
 
-		hit.t = (t2 > 0) ? t2 : t1;
+		hit.t = (solution.y > 0) ? solution.y : solution.x;
 		hit.position = ray.start + ray.dir * hit.t;
+
 		hit.normal = normalize(gradf(vec4(hit.position.x, hit.position.y, hit.position.z, 1.0f)));
 		hit.material = material;
 		return hit;
 	}
 };
 
-struct Hiperboloid : public Intersectable {
-	vec3 param;
-	mat4 mx;
+struct Hyperboloid : public Quadric {
+	float a, b, c;
 
-	Hiperboloid(vec3 _param) {
-		param = _param;
-		mx = mat4(
-			1.0f / (param.x * param.x), 0.0f, 0.0f, 0.0f,
-			0.0f, 1.0f / (param.y * param.y), 0.0f, 0.0f,
-			0.0f, 0.0f, -1.0f / (param.z * param.z), 0.0f,
+	Hyperboloid(float _a, float _b, float _c, Material* _material) {
+		material = _material;
+		a = _a; b = _b; c = _c;
+		Q = mat4(
+			1.0f / (a * a), 0.0f, 0.0f, 0.0f,
+			0.0f, 1.0f / (b * b), 0.0f, 0.0f,
+			0.0f, 0.0f, -1.0f / (c * c), 0.0f,
 			0.0f, 0.0f, 0.0f, -1.0f
 		);
 	}
 };
 
-struct Hyperboloid : public Intersectable {
-	vec3 param;
-	Quadrics mx;
+struct EllipticalCone : public Quadric {
+	float a, b, c;
 
-	Hyperboloid(float _a, float _b, float _c, Material* _material) {
-		param = vec3(_a, _b, _c);
+	EllipticalCone(float _a, float _b, float _c, Material* _material) {
 		material = _material;
-		mx = Quadrics(mat4(
-			1.0f / (param.x * param.x), 0.0f, 0.0f, 0.0f,
-			0.0f, 1.0f / (param.y * param.y), 0.0f, 0.0f,
-			0.0f, 0.0f, -1.0f / (param.z * param.z), 0.0f,
-			0.0f, 0.0f, 0.0f, -1.0f
-		));
-	}
-
-	Hit intersect(const Ray& ray) {
-		Hit hit;
-		
-		vec2 solution = mx.solve(ray);
-		float t1 = solution.x;
-		float t2 = solution.y;
-		if (t1 <= 0) return hit;
-
-		hit.t = (t2 > 0) ? t2 : t1;
-		hit.position = ray.start + ray.dir * hit.t;
-		hit.normal = normalize(mx.gradf(vec4(hit.position.x, hit.position.y, hit.position.z, 1.0f)));
-		hit.material = material;
-		return hit;
+		a = _a; b = _b; c = _c;
+		Q = mat4(
+			1.0f / (a * a), 0.0f, 0.0f, 0.0f,
+			0.0f, 1.0f / (b * b), 0.0f, 0.0f,
+			0.0f, 0.0f, -1.0f / (c * c), 0.0f,
+			0.0f, 0.0f, 0.0f, 0.0f
+		);
 	}
 };
 
-struct Ellipsoid : public Intersectable {
-	float a, b, c; // distances from center
-	Quadrics quad;
+struct EllipticParaboloid : public Quadric {
+	float a, b;
+
+	EllipticParaboloid(float _a, float _b, Material* _material) {
+		a = _a; b = _b;
+		material = _material;
+		Q = mat4(
+			1.0f / (a * a), 0.0f, 0.0f, 0.0f,
+			0.0f, 1.0f / (b * b), 0.0f, 0.0f,
+			0.0f, 0.0f, 0.0f, 0.0f,
+			0.0f, 0.0f, 0.0f, 0.0f
+		);
+	}
+};
+
+struct Ellipsoid : public Quadric {
+	float a, b, c;
 
 	Ellipsoid(float _a, float _b, float _c, Material* _material) {
-		a = _a;
-		b = _b;
-		c = _c;
+		a = _a; b = _b; c = _c;
 		material = _material;
-		quad = Quadrics(mat4(
+		Q = mat4(
 			1.0f / (a * a), 0.0f, 0.0f, 0.0f,
 			0.0f, 1.0f / (b * b), 0.0f, 0.0f,
 			0.0f, 0.0f, 1.0f / (c * c), 0.0f,
 			0.0f, 0.0f, 0.0f, -1.0f
-		));
-	}
-
-	Hit intersect(const Ray& ray) {
-		Hit hit;
-		/*
-		float d1 = ray.dir.x;
-		float d2 = ray.dir.y;
-		float d3 = ray.dir.z;
-
-		float s1 = ray.start.x;
-		float s2 = ray.start.y;
-		float s3 = ray.start.z;
-
-		float p1 = center.x;
-		float p2 = center.y;
-		float p3 = center.z;
-
-		float b2c2 = b * b * c * c;
-		float a2c2 = a * a * c * c;
-		float a2b2 = a * a * b * b;
-
-		float A = (d1 * d1 * b2c2) + (d2 * d2 * a2c2) + (d3 * d3 * a2b2);
-		float B = 2 * (
-			(d1 * (s1 - p1) * b2c2) +
-			(d2 * (s2 - p2) * a2c2) +
-			(d3 * (s3 - p3) * a2b2)
-			);
-		float C = -(a * a * b * b * c * c) +
-			(s1 - p1) * (s1 - p1) * b2c2 +
-			(s2 - p2) * (s2 - p2) * a2c2 +
-			(s3 - p3) * (s3 - p3) * a2b2;
-
-		float discr = B * B - 4.0f * A * C;
-		if (discr < 0) return hit;
-		float sqrt_discr = sqrtf(discr);
-		float t1 = (-B + sqrt_discr) / 2.0f / A;	// t1 >= t2 for sure
-		float t2 = (-B - sqrt_discr) / 2.0f / A;
-		*/
-		//hit.normal.x = 2.0f * (hit.position.x - center.x) / (a * a);
-		//hit.normal.y = 2.0f * (hit.position.y - center.y) / (b * b);
-		//hit.normal.z = 2.0f * (hit.position.z - center.z) / (c * c);
-		//hit.normal = normalize(hit.normal);
-
-		vec2 solution = quad.solve(ray);
-		float t1 = solution.x;
-		float t2 = solution.y;
-		
-		if (t1 <= 0) return hit;
-		hit.t = (t2 > 0) ? t2 : t1;
-		hit.position = ray.start + ray.dir * hit.t;
-		hit.normal = normalize(quad.gradf(vec4(hit.position.x, hit.position.y, hit.position.z, 1.0f)));
-		//hit.normal = -1.0f * hit.normal;
-
-		hit.material = material;
-		return hit;
+		);
 	}
 };
 
@@ -279,13 +207,13 @@ class Scene {
 	vec3 La;
 public:
 	void build() {
-		vec3 eye = vec3(0.0f, 0.0f, 2.0f), vup = vec3(0, 1, 0), lookat = vec3(0, 0, 0);
+		vec3 eye = vec3(2.0f, 2.0f, 0.0f), vup = vec3(0, 1, 0), lookat = vec3(0, 0, 0);
 		float fov = 45 * M_PI / 180;
 		camera.set(eye, lookat, vup, fov);
 
 		La = vec3(0.4f, 0.4f, 0.4f);
 		// La = vec3(135.0f/255.0f, 206.0f / 255.0f, 235.0f / 255.0f); Sky blue
-		vec3 lightDirection(0.0f, 0.5f, 0.8f), Le(2, 2, 2);
+		vec3 lightDirection(10.0f, 10.5f, 10.8f), Le(2, 2, 2);
 		lights.push_back(new Light(lightDirection, Le));
 
 		vec3 kd1(0.3f, 0.2f, 0.1f), kd2(0.1f, 0.2f, 0.3f), ks(2, 2, 2);
@@ -296,7 +224,9 @@ public:
 			objects.push_back(new Sphere(vec3(rnd() - 0.5f, rnd() - 0.5f, rnd() - 0.5f), rnd() * 0.1f, material2));
 		}
 		objects.push_back(new Ellipsoid(0.3f, 0.5f, 0.4f, material1));
-		objects.push_back(new Hyperboloid(0.45f, 0.65f, 0.55f, material1));
+		objects.push_back(new Hyperboloid(0.45f, 0.65f, 0.25f, material1));
+		//objects.push_back(new EllipticalCone(0.45f, 0.65f, 0.25f, material1));
+		//objects.push_back(new EllipticParaboloid(0.45f, 0.65f, material1));
 	}
 
 	void render(std::vector<vec4>& image) {
